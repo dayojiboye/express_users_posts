688c7618cdaa08fa4696c3b471b332c724d59645
dayojiboye <my@personal>
2024-02-06 17:11:53 +0100

    feat: updated api base url

--- src/routes/authRoutes.ts
+++ src/routes/authRoutes.ts
@@ -1,10 +1,11 @@
 import express from "express";
 import { login, register } from "../controllers/authController";
+import apiBaseUrl from "../config/apiBaseUrl";
 
 const router = express.Router();
 
-router.post("/register", register);
+router.post(`${apiBaseUrl}/register`, register);
 
-router.post("/login", login);
+router.post(`${apiBaseUrl}/login`, login);
 
 export default router;
